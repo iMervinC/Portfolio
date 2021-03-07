312e11fac44ab9ef3b3293ea9bea7d02ce6a583a
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-03-07 22:46:00 +0800

    Finalization on tech cards

--- src/components/UI/ProjCard.tsx
+++ src/components/UI/ProjCard.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 const ProjCard = ({ title, tech }) => {
   return (
-    <div className="flex flex-col sm:flex-row sm:space-x-5 cursor-pointer bg-custom-translucent hover:bg-custom p-2 rounded-md text-black">
-      <div>
-        <h3 className="text-3xl">{title}</h3>
-        <div className="flex flex-col bg-blue-500 text-custom-offwhite p-2 rounded-md">
-          <span>#frontend</span>
-          <span className="flex items-center justify-center gap-2 bg-custom-offwhite p-1 rounded-md text-black">
-            {tech.frontend.map((tech) => (
-              <img
-                className="w-10 h-10"
-                src={`/logos/${tech}.svg`}
-                alt={tech}
-              />
-            ))}
-          </span>
-        </div>
+    <div className="flex flex-col cursor-pointer bg-custom-translucent hover:bg-custom p-4 rounded-md hover:text-black text-custom-offwhite transition-colors duration-500 ease-in-out">
+      <h3 className="text-3xl">{title}</h3>
+      <div className="flex flex-col sm:flex-row gap-2">
+        <TechCard tech="frontend" data={tech.frontend} />
+        <TechCard tech="backend" data={tech.backend} />
       </div>
     </div>
   )
 }
 
-export { ProjCard }
+const TechCard: FC<{ tech: 'frontend' | 'backend'; data: string[] }> = ({
+  tech,
+  data,
+}) => {
+  return (
+    <div
+      className={`flex flex-col text-custom-offwhite p-2 rounded-md sm:w-1/2 transform hover:scale-105 transition-transform ${
+        tech === 'frontend'
+          ? 'bg-blue-500'
+          : tech === 'backend' && 'bg-purple-800'
+      }`}
+    >
+      <span className="">{`${
+        tech === 'frontend' ? '#frontend' : tech === 'backend' && '#backend'
+      }`}</span>
+      <span className="flex flex-wrap items-center justify-center gap-2 bg-custom-offwhite p-1 rounded-md text-black">
+        {data.map((_tech, index) => (
+          <img
+            key={index}
+            className="w-6 h-6"
+            src={`/logos/${_tech}.svg`}
+            alt={_tech}
+          />
+        ))}
+      </span>
+    </div>
+  )
+}
+
+export { ProjCard, TechCard }
--- src/components/Widget/Featured.tsx
+++ src/components/Widget/Featured.tsx
@@ -5,8 +5,9 @@ export const Featured = () => {
   const data = projects
   return (
     <div id="featured" className="my-20">
-      <h2 className="text-6xl">Featured Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  py-5">
+      <h2 className="text-5xl sm:text-6xl text-custom">Featured Projects</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 py-5">
         {data.map((proj, index) => (
           <ProjCard key={index} {...proj} />
         ))}
